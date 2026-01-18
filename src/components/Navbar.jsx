1475c66c200af46db9e865c93639add04e2a2451
Md Shihab Sumon <shihabsumon585@gmail.com>
2026-01-18 11:37:33 +0600

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,36 @@
+"use client";
+import Link from 'next/link';
 import React from 'react'
+// import logo from "/logo.png";
 
 const Navbar = () => {
+
+  const user = { email: "shihabsumon585@gmail.com" || null };
+  console.log(user?.email)
+
+  const links = (user?.email ?
+    <>
+      <li><Link href={"/"}>Home</Link></li>
+      <li><Link href={"/products"}>Products</Link></li>
+      <li><Link href={"/about"}>About</Link></li>
+      <li><Link href={"/faq"}>FAQ</Link></li>
+      <li><Link href={"/contact"}>Contact</Link></li>
+
+      <li><Link href={"/dashboard"}>Dashboard</Link></li>
+      <li><Link href={"/profile"}>Profile</Link></li>
+      <li><Link href={"/settings"}>Settings</Link></li>
+    </>
+    :
+    <>
+      <li><Link href={"/"}>Home</Link></li>
+      <li><Link href={"/products"}>Products</Link></li>
+      <li><Link href={"/about"}>About</Link></li>
+      <li><Link href={"/faq"}>FAQ</Link></li>
+      <li><Link href={"/contact"}>Contact</Link></li>
+    </>)
+
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-100 shadow-sm px-6">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -11,36 +39,20 @@ const Navbar = () => {
           <ul
             tabIndex="-1"
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li><a>Item 1</a></li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
+            {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <Link href={"/"}>
+          <img src={"/logo.png"} className='w-20' alt="" />
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li><a>Item 1</a></li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 w-40 z-1">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </details>
-          </li>
-          <li><a>Item 3</a></li>
+          {links}
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <Link href={"/login"} className='btn btn-primary'>Login</Link>
       </div>
     </div>
   )
